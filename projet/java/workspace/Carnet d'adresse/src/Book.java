796abc29c1fcf76e45749c3ed337101a7fac68a5
zerbino <r2mart10@gmail.com>
2013-02-04 19:04:12 +0100

    ajout des sources uniquement

---
+++ projet/java/workspace/Carnet d'adresse/src/Book.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ws.rs.Path;
+
+@Path("book")
+public class Book {
+	
+	private HashMap<Integer, Contact> contacts;
+	
+	
+	public Book(){
+		this.contacts = new HashMap<Integer, Contact>();
+	}
+	
+	
+	public void addContact(Contact c){
+		this.contacts.put(c.getId(), c);
+	}
+	
+	public List<Contact> getAll(){
+		Iterator<Integer> it = contacts.keySet().iterator();
+		ArrayList<Contact> aRet = new ArrayList<Contact>();
+		while(it.hasNext()){
+			
+			aRet.add(contacts.get(it.next()));
+		}
+		return aRet;
+	}
+	
+	public void editContact(Contact c){
+		contacts.put(c.getId(), c);
+	}
+
+}
