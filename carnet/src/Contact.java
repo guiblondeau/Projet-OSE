81795c0903c6bba6e6cf9ad59de9f28c4e199575
Clarou <clara.migne@gmail.com>
2013-02-06 09:52:29 +0100

    test

---
+++ carnet/src/Contact.java
@@ -0,0 +1,54 @@
+
+
+public class Contact {
+	
+	private int id;
+	private String nom, prenom, numero;
+	
+	
+	public Contact(int id, String nom, String prenom, String numero) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.numero = numero;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public void setNumero(String numero){
+		this.numero = numero;
+	}
+	
+	public String getNumero(){
+		return this.numero;
+	}
+	
+
+	
+	
+	
+	
+
+}
