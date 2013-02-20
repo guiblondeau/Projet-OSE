b4e5b13abe7cafe9b497fec8ae774d88d3a849b8
Guillaume <guillaume@guillaume-VirtualBox.(none)>
2013-02-20 10:12:31 +0100

    Modification de contact.js

--- contact/restify-contact/contact.js
+++ contact/restify-contact/contact.js
@@ -9,14 +9,31 @@ function Contact(){
     this.numero = 0;
 }
 
-var book = [];
+var cpt = 0; // donne la valeur de l'indentifiant
+
+var book = [];// stocke les contacts
+var affichage = [];// sert au renvoi des contacts (cf function getAll)
 
 function getAll(req, res, next){
-    res.send(book);
-    return book;
+    var compteur = 0;
+    for (var id in book){
+	if (book[id] != null){
+	    affichage[compteur]=book[id];
+	    compteur++;
+	}
+    }
+    res.send(affichage);
 }
 
-function editContact(){
+function editContact(req, res, next){
+    var contact = new Contact();
+    var param = req.params;
+    contact.nom = param.nom;
+    contact.prenom = param.prenom;
+    contact.id = param.id;
+    contact.numero = param.numero;
+    res.send(req.params);
+    book[param.identifiant] = contact;
 }
 
 function addContact(req, res, next) {
@@ -27,13 +44,20 @@ function addContact(req, res, next) {
     var param = req.params;
     contact.nom = param.nom;
     contact.prenom = param.prenom;
-    contact.id = param.id;
+    contact.id = cpt;
     contact.numero = param.numero;
-    book.push(contact);
+    book[cpt] = contact;
+    cpt = cpt+1;
 }
 
-server.post('/book/addContact', addContact);
-server.post('/book/getAll', getAll);
-server.post('/book/editContact', editContact);
+function deleteContact(req, res, next){
+    book[req.params.identifiant] = null;
+    res.send ("Contact supprimé =)");
+}
 
+
+server.post('/book/addContact', addContact);
+server.get('/book/getAll', getAll);
+server.put('/book/editContact/:identifiant', editContact);
+server.del('book/deleteContact/:identifiant',deleteContact);
 server.listen(8080);
