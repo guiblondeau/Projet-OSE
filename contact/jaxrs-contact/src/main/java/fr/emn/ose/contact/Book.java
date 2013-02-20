20b2b44f82a39f71e401583798070fe88665eecd
zerbino <r2mart10@gmail.com>
2013-02-20 11:41:48 +0100

    API java terminee

--- contact/jaxrs-contact/src/main/java/fr/emn/ose/contact/Book.java
+++ contact/jaxrs-contact/src/main/java/fr/emn/ose/contact/Book.java
@@ -1,5 +1,11 @@
+/**
+ * This class represents a Book containing contacts. Contacts are represented by Contact instances
+ * which can be added, updated and deleted
+ */
+
 package fr.emn.ose.contact;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -7,24 +13,53 @@ import java.util.List;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/book")
+
+
+@Path("/contacts")
 public class Book {
 
     private HashMap<Integer, Contact> contacts;
+    private int compt = 0;
 
     public Book() {
         this.contacts = new HashMap<Integer, Contact>();
     }
 
-    @POST()
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Path("/addContact")
-    public void addContact(Contact c){
-        this.contacts.put(c.getId(), c);
+    /**
+     * Generates a new id.
+     *
+     * Generate a new id by incrementing the former value of id
+     * @return the new id
+     */
+    public int inc(){
+        this.compt++;
+        return compt;
     }
 
+
+    /**
+     * Adds the contact to the list
+     * @param c : the contact to be added
+     * @return the contact that was added
+     */
     @POST()
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/addContact")
+    public Contact addContact(Contact c){
+        c.setId(this.inc());
+        this.contacts.put(c.getId(), c);
+        return c;
+    }
+
+    /**
+     * Return all the contacts which were added to the book.
+     *
+     * @return the list of contacts to be displayed
+     */
+    @GET()
     @Produces({MediaType.APPLICATION_JSON})
     @Path("/getAll")
     public List<Contact> getAll(){
@@ -38,17 +73,33 @@ public class Book {
         return aRet;
     }
 
+
+    /**
+     * Edit the contact in the book with the new value.
+     *
+     * @param id  the id of the contact to be updated
+     * @param c   the contact that overrides the former value
+     * @return    the new contact that overrides the former value
+     */
     @PUT()
-    @Path("/editContact")
+    @Path("/editContact/{contact}")
     @Consumes({MediaType.APPLICATION_JSON})
-    public void editContact(Contact c){
-        contacts.put(c.getId(), c);
+    @Produces({MediaType.APPLICATION_JSON})
+    public Contact editContact(@PathParam("contact")String id, Contact c){
+        contacts.put(Integer.parseInt(id), c);
+        return c;
     }
 
+
+    /**
+     *
+     * @param id
+     * @return
+     */
     @DELETE()
-    @Path("/delete")
-    @Consumes({MediaType.APPLICATION_JSON})
-    public void deleteContact(Contact contact){
-        this.contacts.remove(contact.getId());
+    @Path("/editContact/{contact}")
+    public Response deleteContact(@PathParam("contact") String id){
+        this.contacts.remove(Integer.parseInt(id));
+        return Response.status(200).build();
     }
 }
