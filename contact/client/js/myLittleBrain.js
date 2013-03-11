1bb5a036d8d593b988427d027f21a1f1913d13f0
zerbino <r2mart10@gmail.com>
2013-03-11 21:51:25 +0100

    client avancement

--- contact/client/js/myLittleBrain.js
+++ contact/client/js/myLittleBrain.js
@@ -1,20 +1,38 @@
 (function($) {
 
+
         window.Contact = Backbone.Model.extend({
 
             defaults : {
                 id : "???",
 				prenom:"j'ai pas de nom",
 				nom:"j'ai pas de prenom",
-                numero:"911"
+                numero:"911",
+				validate:true
             },
 
             initialize : function() {
                 console.log('Contact Constructor');
-				
+				this.on("invalid", function(model, error){
+				console.log( error );
+			});
 				
 				
             },
+			
+			setPrenom : function(value){
+				this.set({prenom:value});
+			},
+			
+			setNom : function(value){
+				this.set({nom:value});
+			},
+			
+			validate : function(attributes){
+				if(attributes.prenom ==='' ||attributes.nom === ''){
+					return "Le nom et le prenom ne peuvent être vides";
+				}
+			}
 			
 			
 			
@@ -22,20 +40,15 @@
 			
         });
 
-
-    })(Zepto);
-	
-	
-	(function($) {
-
         
 
       window.Book = Backbone.Collection.extend({
 		model : Contact,
+		name : "book",
 		localStorage : new Store("contact"),
 		
 		initialize : function(){
-			this.url = "http://localhost:8080/jaxrs-service/book/getAll"
+			this.url = "jaxrs-service/book/getAll"
 			console.log('Book constructor');
 		}
 		
@@ -43,18 +56,26 @@
 	  
 	  });
 	  
-	  window.DocView = Backbone.View.extend({
+	  window.BookView = Backbone.View.extend({
         el : $('#contact-container'),
         initialize : function() {
-            this.template = _.template($('#contact-template').html());
+			log("BookView constructor");
+            this.template = $('#contact-template').html();
+			
+			
+			_.bindAll(this, 'render');
+            this.collection.bind('change', this.render);
+            this.collection.bind('add', this.render);
+            this.collection.bind('remove', this.render);
         },
 
         render : function() {
-            var renderedContent = this.template(this.model.toJSON());
-            $(this.el).html(renderedContent);
+            var renderedContent = Mustache.to_html(this.template, this.collection.toJSON());
+			$(this.el).html(renderedContent);
             return this;
         }
 
     });
+
 
     })(Zepto);
