800c6cf8c380ab111c407fde6ef1236a2fc73e68
zerbino <r2mart10@gmail.com>
2013-03-19 21:37:14 +0100

    update des contacts

--- contact/jaxrs-contact/client/workspace/client-rest/js/Book.js
+++ contact/jaxrs-contact/client/workspace/client-rest/js/Book.js
@@ -3,7 +3,9 @@
 			var BookJS = {};
 			window.BookJS = BookJS;
 			
-			var hashTemplate = {'contactList':window.contactListTemplate, 'contactEdit':window.contactEditTemplate, 'contactAdd':window.contactAddTemplate};
+			
+			
+			var hashTemplate = {'contact':window.contactTemplate,'contactList':window.contactListTemplate, 'contactEdit':window.contactEditTemplate, 'contactAdd':window.contactAddTemplate};
 			
 			 var template = function(name) {
 			    return Mustache.compile(hashTemplate[name]);
@@ -14,7 +16,7 @@
         
 
             defaults : {
-                id :'',
+                
 				prenom:"j'ai pas de nom",
 				nom:"j'ai pas de prenom",
                 numero:"911",
@@ -66,42 +68,113 @@
 	  
 	  });
 	  
+	  BookJS.collection = new BookJS.Book();
+	  
+	  
+	  
+	  
+	  
 	  BookJS.BookView = Backbone.View.extend({
         template : template('contactList'),
         
-        
-        
+    
         initialize : function() {
 			console.log("BookView constructor");
 			
 			
 			
-			this.collection = new BookJS.Book();
-            this.collection.on('all', this.render, this);
-            this.collection.fetch();
+            BookJS.collection.on('all', this.render, this);
+            BookJS.collection.fetch();
             
+        },
+        
+       render : function() {
+
+        
+			
+			
+            var renderedContent = this.template(); 
+			
+			
+			
+			$(this.el).html(renderedContent);
+			BookJS.collection.each(this.addContact, this);
+			$(this.el).append('<div id = "connerie"></div>');
+			
+			addForm = new BookJS.AddForm({collection:BookJS.collection});
+			$(this.el).append(addForm.render().el);
+            return this;
+        },
+        
+        addContact:function(contact){
+        
+        	contact =new BookJS.contactView({model:contact});
+        	
+        	$(this.el).find('#contactsContainer').append(contact.render().el);
+        }
+		
+		
+    });
+    
+    BookJS.contactView = Backbone.View.extend({
+    	template : template('contact'),
+    	
+    	events :{
+    		'click .img':'update'
+    	},
+    	
+    	
+    	
+    	render:function(){
+    	window.templateVisible = this.template;
+    		
+    		
+    		renderedContent = this.template(this.model.toJSON());
+    		$(this.el).html(renderedContent);
+    		return this;
+    	},
+    	
+    	update:function(){
+    		BookJS.router.navigate('#editContact/'+this.model.id, {trigger:true});
+    	}
+    	
+    	
+    	
+    	
+    });
+    
+    
+     BookJS.editView = Backbone.View.extend({
+        template : template('contactEdit'),
+        contactID:0,
+        
+        events:{
+        	'click button#deleteContact' : 'removeContact'
+        },
+        
+        
+        initialize : function() {
+			console.log("BookView constructor");
+			
+			         
         },
 
 		
 			
         render : function() {
-
-        
-			var val2 = {contactList : this.collection.toJSON()};
 			
 			
-			
-            var renderedContent = this.template(val2); 
-			
-			if(this.collection.length ==1){
-				console.log(JSON.stringify(val2));
-			}
-			
+            var renderedContent = this.template(BookJS.collection.get(this.contactID)); 
+            		
 			$(this.el).html(renderedContent);
-			addForm = new BookJS.AddForm({collection:this.collection});
-			$(this.el).append(addForm.render().el);
             return this;
         },
+        
+        
+        removeContact:function(){
+        	BookJS.collection.get(this.contactID).destroy();
+        	BookJS.router.navigate('/',{pushstate:true});
+        }
 		
 		
     });
@@ -118,17 +191,16 @@
     },
     add: function(event) {
      event.preventDefault();
-     
-     this.collection.create({nom: $('#nom').val(), prenom:$('#prenom').val(), numero:$('#numero').val()})?console.log('success'):console.log('failure');
+     this.collection.create({nom: $('#nom').val(),prenom:$('#prenom').val(),numero:$('#numero').val()});
    	window.kollec = this.collection;
    	this.render();
     
     },
-    bidon:function(){
-    	console.log('ici ça passe');
-    }
+    
     
   });
+  
+  
   
    
    
@@ -139,19 +211,27 @@
 	      this.el = options.el
 	    },
 	    routes: {
-	      "": "index"
+	      "": "index",
+	      "editContact/:id" : "edit"
 	    },
 	    index: function() {
 	      var index = new BookJS.BookView();
 	      this.el.empty();
 	      this.el.append(index.render().el);
+	    },
+	    
+	    edit:function(id){
+	    	this.el.empty();
+	    	editView = new BookJS.editView();
+	    	editView.contactID=id;
+	    	this.el.append(editView.render().el);
 	    }
 	  });
     
     BookJS.boot = function(container){
 		container = $(container);
-		var router = new BookRouter({el:container});    
-		Backbone.history.start();	
+		BookJS.router = new BookRouter({el:container});    
+		Backbone.history.start({pushstate:true});	
     
     }
     
