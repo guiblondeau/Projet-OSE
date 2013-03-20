8608c333052e781de2695bd270967bee53bdb7c5
guiblondeau <blondeau.gui@gmail.com>
2013-03-20 13:19:53 +0100

    Correct bug in Book.java in addContactOptions

--- contact/jaxrs-contact/src/main/java/fr/emn/ose/contact/Book.java
+++ contact/jaxrs-contact/src/main/java/fr/emn/ose/contact/Book.java
@@ -76,7 +76,7 @@ public class Book {
     public Response addContactOption() {
         return Response.ok().
                 header("Access-Control-Allow-Origin", "*").
-                header("Access-Control-Allow-Methods", "POST, OPTIONS").
+                header("Access-Control-Allow-Methods", "POST", "OPTIONS").
                 header("Access-Control-Allow-Headers", "Content-Type").build();
     }
 
