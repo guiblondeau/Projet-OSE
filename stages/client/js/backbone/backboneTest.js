9ba4aa9c3bd1e2f55fb4d024dd1e507ec8ae3d34
guiblondeau <blondeau.gui@gmail.com>
2013-04-24 09:54:06 +0200

    Ajout fonctionnalite google maps avec geolocalisation

--- stages/client/js/backbone/backboneTest.js
+++ stages/client/js/backbone/backboneTest.js
@@ -13,7 +13,9 @@
 			salaire:"0€",
 			option:"gsi bien sur",
 			avantages:"aucun",
-			langue:"francais sauf si t'as pas l'IELTS"
+			langue:"francais sauf si t'as pas l'IELTS",
+			latitude : 0,
+			longitude : 0,
 		},
 	});
 
@@ -97,25 +99,40 @@
 		},
 
 		addStage : function(){
-			var stage = new Stage({
-				id : counter,
-				intitule : $('#intitule').val(),
-				entreprise : $('#entreprise').val(),
-				pays : $('#pays').val(),
-				domaine : $('#domaine').val(),
-				option : $('#option').val(),
-				description : $('#description').val(),
-				adresse : $('#adresse').val(),
-				salaire : $('#salaire').val(),
-				avantages : $('#avantages').val(),
-				langue : $('#langue').val(),
-			});
-			counter++;
-			console.log(counter);
-			console.log(stage);
-			collection.add(stage);
-			$('#intitule').empty();
-			index.render();
+			var geocoder = new google.maps.Geocoder();
+			var address = $('#adresse').val();
+			geocoder.geocode( { 'address': address}, function(results, status) {
+  				if (status == google.maps.GeocoderStatus.OK) {
+   					var latitude = results[0].geometry.location.lat();
+    				var longitude = results[0].geometry.location.lng();
+    				var stage = new Stage({
+						id : counter,
+						intitule : $('#intitule').val(),
+						entreprise : $('#entreprise').val(),
+						pays : $('#pays').val(),
+						domaine : $('#domaine').val(),
+						option : $('#option').val(),
+						description : $('#description').val(),
+						adresse : $('#adresse').val(),
+						salaire : $('#salaire').val(),
+						avantages : $('#avantages').val(),
+						langue : $('#langue').val(),
+						latitude : latitude,
+						longitude : longitude,
+					});
+					counter++;
+					console.log(stage.get("longitude"));
+					collection.add(stage);
+					$('#intitule').val("");
+					$('#entreprise').val("");
+					$('#pays').val("");
+					$('#domaine').val("");
+					$('#option').val("");
+					$('#description').val("");
+					index.render();
+  				} 
+			}); 
+			
 		},
 
 		editStage : function(id){
@@ -124,9 +141,16 @@
 			$('#page-principale').slideUp();
 			$('#add-stage').slideDown();
 			$('#intitule').val(stage.get("intitule"));
+			$('#entreprise').val(stage.get("entreprise"));
+			$('#description').val(stage.get("description"));
 			$('#pays').val(stage.get("pays"));
 			$('#domaine').val(stage.get("domaine"));
 			$('#option').val(stage.get("option"));
+			$('#description').val(stage.get("description"));
+			$('#adresse').val(stage.get("adresse"));
+			$('#salaire').val(stage.get("salaire"));
+			$('#avantages').val(stage.get("avantages"));
+			$('#langue').val(stage.get("langue"));
 		}
 	});
 
